8528fbe3cac1deb6a1790413862d239a8949766f
Brian Pennington <srsrec@gmail.com>
2018-10-24 15:55:05 -0500

    created most of the classes

---
+++ RockPapSciLizSpock/RockPapSciLizSpock/PlayerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPapSciLizSpock
+{
+    abstract class PlayerModel
+    {
+        public string playerName;
+        public int playerScore;
+
+        public PlayerModel()
+        {
+
+        }
+
+        public abstract void PlayerMakesMove();
+        
+    }
+}
